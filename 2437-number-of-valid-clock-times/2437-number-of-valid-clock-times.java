7c94162f4add69ba1e18c44d46e87ae77d5c2c24
Vishal Mishra <79922690+vishalmishra2810@users.noreply.github.com>
2022-10-16 00:40:05 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (50.00%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countTime(String time) {
+        
+        int prod = 1;
+        
+        // if(time.charAt(0) == '?' && time.charAt(1) == '?' && time.charAt(3) == '?' && time.charAt(4) == '?'){
+        //     return 1440;
+        // }
+        
+        if(time.charAt(0) == '?'){
+            if(time.charAt(1) >= '4' && time.charAt(1) <= '9'){
+                prod *= 2;
+            }
+            else{
+                prod *= 3;
+            }   
+        }
+            
+            if(time.charAt(1) == '?'){
+                if(time.charAt(0) == '?'){
+                    prod = 24;
+                }
+                else if(time.charAt(0) == '2'){
+                    prod *= 4;
+                }
+                else{
+                    prod *= 10;
+                }
+            }       
+                if(time.charAt(4) == '?'){
+                    prod *= 10;
+                }
+                if(time.charAt(3) == '?'){
+                    prod *= 6;
+                }
+        
+        
+        return prod;
+        
+    }
+}
