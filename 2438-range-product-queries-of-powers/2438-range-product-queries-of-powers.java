da6992ec6136b55fb70dfdc31951aed22d22e1d1
Vishal Mishra <79922690+vishalmishra2810@users.noreply.github.com>
2022-10-16 01:00:06 +0530

    Time: 61 ms (83.33%), Space: 88.5 MB (100.00%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -2,16 +2,17 @@ class Solution {
     public int[] productQueries(int n, int[][] queries) {
         
         int mod=1000000007;
-        //generating the array based on the given n value  n%2 and n/2 evry time genrating an array of 0 and 1 indcating whether to consdier the index or not and generate the powers array according to the array generated indexes
+       
         List<Integer> arr=new ArrayList<Integer>();
         List<Integer> powers=new ArrayList<Integer>();
         while(n>0){  
             arr.add((int)n%2);
             n/=2;
         }
-         //System.out.print(arr);
+      
         for(int i=0;i<arr.size();i++){
-            if(arr.get(i)==1){  //considering only the arrays of value equals 1 indicating the maximum powers sum to n
+            if(arr.get(i)==1){ 
+                
                 int t=(int)Math.pow(2,i);
                 powers.add(t);
             }
