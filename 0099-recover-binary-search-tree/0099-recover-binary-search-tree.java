2af8fd6f6aa491f46dc23988cca3137b4cb73509
Vishal Mishra <79922690+vishalmishra2810@users.noreply.github.com>
2022-11-10 12:40:06 +0530

    Time: 4 ms (51.84%), Space: 47.4 MB (36.47%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,36 @@
+class Solution {
+    private TreeNode first = null;
+    private TreeNode second = null;
+    private TreeNode pre = null;
+    
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+
+        //find swapped nodes
+        inorder(root);
+        
+        //swap the nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        //find the first node which is at wrong position
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        
+        //find the second node which is at wrong position
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        
+        //store previus node to compare
+        pre = root;
+        inorder(root.right);
+    }
+}
