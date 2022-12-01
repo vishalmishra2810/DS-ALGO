965065920678f82ee4d03f7c6eafbce2df1c82ad
Vishal Mishra <79922690+vishalmishra2810@users.noreply.github.com>
2022-12-01 19:38:58 +0530

    Time: 2 ms (99.76%), Space: 42.6 MB (83.65%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,36 +1,46 @@
 class Solution {
-    private TreeNode first = null;
-    private TreeNode second = null;
-    private TreeNode pre = null;
+     // Creating three global pointers
+    TreeNode previous = null;
+    TreeNode first = null;
+    TreeNode second = null;
     
     public void recoverTree(TreeNode root) {
-        if(root==null) return;
-
-        //find swapped nodes
-        inorder(root);
+        // Finding the two swapped nodes
+        solve(root);
         
-        //swap the nodes
+        //Swaping the value of nodes
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
     }
-    
-    private void inorder(TreeNode root){
-        if(root==null) return;
-        inorder(root.left);
+     //New Function to find the two nodes
+    public void solve(TreeNode root){
         
-        //find the first node which is at wrong position
-        if(first==null && (pre==null ||pre.val>=root.val)){
-            first = pre;
+        if(root == null){
+            return;
         }
         
-        //find the second node which is at wrong position
-        if(first!=null && pre.val>=root.val){
+        //Doing inorder traversal i.e, sarting from left then right
+        solve(root.left);
+        
+        // If current node is smaller than previous, then previous node is invalid
+        if(previous != null && root.val < previous.val){
+            
+            //Storing previous node
+            if(first == null){
+                first = previous;
+            }
+            
+            //If both nodes are adjacent, save the current node in that case
             second = root;
+            
         }
         
-        //store previus node to compare
-        pre = root;
-        inorder(root.right);
+        //Making current node as previous node
+        previous = root;
+        
+        //Moving to right sub-tree
+        solve(root.right);
+        
     }
 }
